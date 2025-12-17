720ea9ed24f7894f0e9a7e67d3b6439679e9cb8c
Schirrer Sebastian <sebastian.schirrer@hotmail.com>
2025-12-17 16:54:38 +0100

    backend ready

--- foto_challenge_backend/src/seed.js
+++ foto_challenge_backend/src/seed.js
@@ -2,13 +2,14 @@
 import { pool } from "./db.js";
 
 export async function runSeed() {
-    const { rowCount } = await pool.query(`SELECT * FROM user;`);
-    if (rowCount === 0) {
+    const { rowCount } = await pool.query(`SELECT * FROM User;`);
+    if (rowCount < 10) {
+        // console.log(rowCount);
         await pool.query(`
-      INSERT INTO user (name, email) VALUES
-        ('Alice', 'alice@example.com'),
-        ('Bob', 'bob@example.com');
-    `);
+      INSERT INTO "User" (Username, Passwort) VALUES
+    ('Alice', '123'),
+    ('Bob', '1234');
+`);
         console.log("✔ Testdaten eingefügt");
     } else {
         console.log("✔ Testdaten bereits vorhanden");
